56c556a7211dd6ad534ac23597f4b7ce6d6e48ce
JekicVladimir <jekicvladimir9@gmail.com>
2021-10-28 14:51:32 +0200

    na ti sve

--- src/main/java/com/Email/EmailApplication.java
+++ src/main/java/com/Email/EmailApplication.java
@@ -2,12 +2,16 @@ package com.Email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class EmailApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
 	}
-
 }
+
+
+
