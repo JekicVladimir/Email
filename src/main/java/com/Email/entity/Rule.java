06af7c3d43dbb34dbd912f1d96f6c73862e79c97
JekicVladimir <jekicvladimir9@gmail.com>
2021-11-01 15:54:13 +0100

    11 1

---
+++ src/main/java/com/Email/entity/Rule.java
@@ -0,0 +1,21 @@
+package com.Email.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Entity
+@Table(name = "rule")
+public class Rule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private int condition;
+    @Column
+    private String value;
+    @Column
+    private int operation;
+}
